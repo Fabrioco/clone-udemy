a2ed4de2173b5d61fd357ba4f88a5fe79ab50a5f
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-10-30 11:27:29 -0300

    :books: docs: configuração tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        primary: ["Abril Fatface", "serif"],
-        secondary: ["Taviraj", "serif"],
-        tertiary: ["Alegreya Sans", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
