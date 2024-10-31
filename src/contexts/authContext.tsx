79ce9b88448ae8fba26af8f783b4ee1ed5469fe9
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-10-31 18:58:06 -0300

    :sparkles: feat: sistema de login e redirecionamento para a pagina de dashboard

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { auth, db } from "../services/firebaseConnection";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { FirebaseError } from "firebase/app";
 import { useNavigate } from "react-router-dom";
 
 type AuthContextType = {
   signUp: (name: string, email: string, password: string) => Promise<void>;
+
+  signIn: (email: string, password: string) => Promise<void>;
 };
 
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
+interface UserDataProps {
+  uid: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
 const AuthContext = React.createContext<AuthContextType>({} as AuthContextType);
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const navigate = useNavigate();
+
+  const [userData, setUserData] = React.useState<UserDataProps>();
+
   const signUp = async (
     name: string,
     email: string,
@@ -35,7 +50,14 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         email,
         password,
       });
+      const data: UserDataProps = {
+        uid,
+        name,
+        email,
+        password,
+      };
       navigate(`/dashboard/${uid}`);
+      setUserData(data);
     } catch (error) {
       if (error instanceof FirebaseError) {
         if (error.code === "auth/email-already-in-use") {
@@ -46,8 +68,32 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
       }
     }
   };
+
+  const signIn = async (email: string, password: string): Promise<void> => {
+    const authResponse = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const uid = authResponse.user.uid;
+    const docSnap = await getDoc(doc(db, "users", uid));
+
+    if (docSnap.exists()) {
+      const data: UserDataProps = {
+        uid: uid,
+        name: docSnap.data()?.name,
+        email: docSnap.data()?.email,
+        password: docSnap.data()?.password,
+      };
+      setUserData(data);
+      navigate(`/dashboard/${uid}`);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ signUp }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signUp, signIn }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
